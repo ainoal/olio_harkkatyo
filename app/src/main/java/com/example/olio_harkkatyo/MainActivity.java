dc7af2e78c57b13baf5031c0a8450562db15cd7a
JFors-LUT <71925242+JFors-LUT@users.noreply.github.com>
2021-04-11 20:28:46 +0300

    CO2 laskuri alottelu

--- app/src/main/java/com/example/olio_harkkatyo/MainActivity.java
+++ app/src/main/java/com/example/olio_harkkatyo/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.olio_harkkatyo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    public void co2Activity (View v){
+        Intent intent = new Intent(MainActivity.this, co2_calculator.class);
+        startActivity(intent);
     }
 }
