a21bb46d3d96bb34648793b035dc187ac04084cb
jennirpa <jenniriikka.aaltonen@gmail.com>
2021-04-28 17:31:56 +0300

    User- ja UserProfile-luokkia päivitetty

--- app/src/main/java/com/example/olio_harkkatyo/User.java
+++ app/src/main/java/com/example/olio_harkkatyo/User.java
@@ -2,5 +2,26 @@ package com.example.olio_harkkatyo;
 
 public class User {
     private String name;
-    private String weight;
+    private float weight;
+
+    User(String name, float weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 }
--- app/src/main/java/com/example/olio_harkkatyo/UserProfile.java
+++ app/src/main/java/com/example/olio_harkkatyo/UserProfile.java
@@ -2,9 +2,12 @@ package com.example.olio_harkkatyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -12,11 +15,32 @@ public class UserProfile extends AppCompatActivity {
     private EditText userWeight;
     private Button applyInfo;
 
+    public static User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
         this.setTitle("Profile information");
+
+        userName = findViewById(R.id.etUserName);
+        userWeight = findViewById(R.id.etUserWeight);
+        applyInfo = findViewById(R.id.btnApplyInfo);
+        applyInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = userName.getText().toString();
+                float weight = Float.valueOf(userWeight.getText().toString());
+
+                user = new User(name, weight);
+                //startActivity( new Intent(UserProfile.this, MainActivity.class)); ??
+                Toast.makeText(UserProfile.this,"User information applied successfully!", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }
     }
 }
