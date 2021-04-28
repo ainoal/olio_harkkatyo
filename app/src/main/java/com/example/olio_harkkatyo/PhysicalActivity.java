c2e299fc6830f0752916b77d40b8d21321ea15e9
ainoal <77739788+ainoal@users.noreply.github.com>
2021-04-28 23:52:56 +0300

    PhysicalActivity

--- app/src/main/java/com/example/olio_harkkatyo/PhysicalActivity.java
+++ app/src/main/java/com/example/olio_harkkatyo/PhysicalActivity.java
@@ -3,44 +3,59 @@ package com.example.olio_harkkatyo;
 import android.content.Context;
 import android.view.View;
 
+import java.util.Scanner;
+
 public class PhysicalActivity {
     String fileName = "testfile.txt";
     String goalFile = "goalfile.txt";
 
-    public void changeGoal(float newGoal, DataManager dm) {
+    public void changeGoal(float newGoal) {
         String inputText = Float.toString(newGoal);
 
+        DataManager dm = DataManager.getInstance();
         dm.writeFile(goalFile, inputText);
     }
 
-    public void SaveDaily(DataManager dm) {
-        String inputText = "DataManager\nTiedoston luku ja kirjoitus\n:)"; // Testi
+    public void SaveDaily(float dailyActivity) {
+        String inputText = Float.toString(dailyActivity);
 
+        DataManager dm = DataManager.getInstance();
         dm.writeFile(fileName, inputText);
     }
 
-    public Integer ActivityToGoal(DataManager dm) {
-        Integer dailyAverage;
-        Integer difference = 0;
-        Integer lineCount = 0;
-        String line;
-        float historicalActivity =  0;
+    public float ActivityToGoal() {       // float goal parametrinä?
+        float dailyAverage;
         float goal;
+        float difference = 0;
+        float historicalActivity = 0;
+        String line;
+        Integer lineCount = 0;
+        String inputText;
+
+        DataManager dm = DataManager.getInstance();
 
         String file = dm.readFile(fileName);
         String file2 = dm.readFile(goalFile);
 
-        /*
-        while (käydään läpi rivit) {
+        Scanner avgScanner = new Scanner(file);
+
+        while (avgScanner.hasNextLine()){
+            line = avgScanner.nextLine();
             historicalActivity += Float.parseFloat(line.trim());
+            lineCount++;
+            }
+
+        if (lineCount != 0) {   // If there's sleep activity data in the file
+            dailyAverage = historicalActivity / lineCount;
+            goal = Float.parseFloat(file2.trim());
+            difference = goal - dailyAverage;
+        } else {
+            return 1000;  // return an impossible number as a sign that the user hasn't input any data
         }
-        dailyAverage = historicalActivity / lineCount;
 
-        goal = Float.parseFloat(file2.trim());
-
-        difference = goal - dailyAverage;
-        System.out.println(difference); // testi
-         */
+        inputText = Float.toString(difference);
+        System.out.print("DIFFERENCE: ");
+        System.out.println(inputText);
 
         return difference;
     }
