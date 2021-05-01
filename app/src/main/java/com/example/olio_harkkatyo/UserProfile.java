0c16185cc5c4dc5564d72c2066246c858d4331cc
jennirpa <jenniriikka.aaltonen@gmail.com>
2021-05-01 18:16:50 +0300

    Syntymäpäivä lisätty UserProfileen

--- app/src/main/java/com/example/olio_harkkatyo/UserProfile.java
+++ app/src/main/java/com/example/olio_harkkatyo/UserProfile.java
@@ -2,18 +2,26 @@ package com.example.olio_harkkatyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class UserProfile extends AppCompatActivity {
 
     private EditText userName;
     private EditText userWeight;
     private Button applyInfo;
+    private DatePickerDialog datePickerDialog;
+    private Button bday;
+
 
     public static User user;
 
@@ -41,5 +49,83 @@ public class UserProfile extends AppCompatActivity {
             }
 
         });
+
+        //datePicker();
+        bday = findViewById(R.id.btnBday);
+        bday.setText(getTodaysDate());
+
+    }
+    private String getTodaysDate(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        month = month + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(year, month, day);
+    }
+    private void datePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1; //Jan = 0
+                String date = makeDateString(year, month, day);
+                bday.setText(date);
+            }
+        };
+
+       Calendar calendar = Calendar.getInstance();
+       int year = calendar.get(Calendar.YEAR);
+       int month = calendar.get(Calendar.MONTH);
+       int day = calendar.get(Calendar.DAY_OF_MONTH);
+       int style = AlertDialog.THEME_HOLO_LIGHT;
+       datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int year, int month, int day){
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month){
+        if(month == 1) {
+            return "JAN";
+        }
+        if(month == 2) {
+            return "FEB";
+        }
+        if(month == 3) {
+            return "MAR";
+        }
+        if(month == 4) {
+            return "APR";
+        }
+        if(month == 5) {
+            return "MAY";
+        }
+        if(month == 6) {
+            return "JUN";
+        }
+        if(month == 7) {
+            return "JUL";
+        }
+        if(month == 8) {
+            return "AUG";
+        }
+        if(month == 9) {
+            return "SEP";
+        }
+        if(month == 10) {
+            return "OCT";
+        }
+        if(month == 11) {
+            return "NOV";
+        }
+        if(month == 12) {
+            return "DEC";
+        }
+        //default (should never happen), makes the red go away
+        return "JAN";
+    }
+    public void openDatePicker(View view){
+        datePickerDialog.show();
     }
 }
