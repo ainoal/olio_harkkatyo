6e823fe4ce0ca455476918f9077bba3ea712c044
ainoal <77739788+ainoal@users.noreply.github.com>
2021-05-04 11:58:11 +0300

    WeightManagement framework

--- app/src/main/java/com/example/olio_harkkatyo/WeightManagement.java
+++ app/src/main/java/com/example/olio_harkkatyo/WeightManagement.java
@@ -4,10 +4,32 @@ public class WeightManagement {
     float weight;
 
     public class IdealWeight extends WeightManagement {
+        float idealWeight;
+
+        public void setIdealWeight(float inputIdeal) {
+            idealWeight = inputIdeal;
+        }
+
+        public float comparison() {
+            float difference = idealWeight - weight;
+            return difference;
+        }
 
     }
 
     public class WeightChange extends WeightManagement {
 
+        public void setWeight(float inputWeight) {
+            weight = inputWeight;
+        }
+
+        public float getChange() { // Change between which exact points in time?
+            float change = 0;
+            return change;
+        }
+
+        public void createHistory() {
+
+        }
     }
 }
